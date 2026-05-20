7e953bdac484c91d36fe2b7dc205c2138d0f21f1
BrianDarakdjian <Braydarak@gmail.com>
2026-05-20 11:54:21 +0200

    feat: add auto-populate for property API endpoints
    - add automatic population of required relations (tourCoverImage, virtualTourFolder with files) to property controller's find and findOne methods
    - update jsconfig.json with nodenext module resolution and resolveJsonModule settings
    - standardize quote style in bootstrap.js to use double quotes consistently
    - add content manager UI configuration setup for property content type and upload folder defaults

--- src/api/property/controllers/property.js
+++ src/api/property/controllers/property.js
@@ -6,4 +6,65 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::property.property');
+const requiredPopulate = {
+  tourCoverImage: true,
+  virtualTourFolder: {
+    populate: {
+      files: true,
+    },
+  },
+};
+
+const mergePopulate = (populate) => {
+  if (!populate) {
+    return { ...requiredPopulate };
+  }
+
+  if (populate === '*' || populate === true) {
+    return populate;
+  }
+
+  if (typeof populate !== 'object' || Array.isArray(populate)) {
+    return populate;
+  }
+
+  return {
+    ...requiredPopulate,
+    ...populate,
+    virtualTourFolder:
+      populate.virtualTourFolder && typeof populate.virtualTourFolder === 'object'
+        ? {
+            ...requiredPopulate.virtualTourFolder,
+            ...populate.virtualTourFolder,
+            populate:
+              populate.virtualTourFolder.populate &&
+              typeof populate.virtualTourFolder.populate === 'object'
+                ? {
+                    ...requiredPopulate.virtualTourFolder.populate,
+                    ...populate.virtualTourFolder.populate,
+                  }
+                : requiredPopulate.virtualTourFolder.populate,
+          }
+        : requiredPopulate.virtualTourFolder,
+  };
+};
+
+module.exports = createCoreController('api::property.property', () => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: mergePopulate(ctx.query?.populate),
+    };
+
+    return await super.find(ctx);
+  },
+
+  async findOne(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: mergePopulate(ctx.query?.populate),
+    };
+
+    return await super.findOne(ctx);
+  },
+}));
